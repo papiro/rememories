215ddf04f9a5ae772d484b24aa2cef59b2ad5c94
Pierre Pirault <pierre.pirault@outlook.com>
2018-02-16 02:23:55 -0500

    Added passport-facebook module

--- server/config/auth.js
+++ server/config/auth.js
@@ -6,7 +6,8 @@ const
 ,
   passport = require('passport'),
   strategies = {
-    google: require('passport-google-oauth').OAuth2Strategy
+    google: require('passport-google-oauth').OAuth2Strategy,
+    facebook: require('passport-facebook').Strategy
   }
   //redisc = require('redis').createClient()
 ;
@@ -29,6 +30,25 @@ exports.init = (app) => {
     res.redirect(`/home`)
     done()
   })
+
+  passport.use(new strategies.facebook({
+    clientID: '186168231982225',
+    clientSecret: '791e34294ca803def327abab9b3e48d5',
+    callbackURL: 'http://rememories.com/auth/facebook'
+  }, (accessToken, refreshToken, profile, done) => {
+    const { id, displayName, emails } = profile;
+    const user = { id, name: displayName, email: emails[0].value }
+    debug(user)
+    const newUser = (new User(user))
+    newUser
+      .then( () => {
+        debug('User added or user exists')
+        done(null, user)
+      })
+      .catch( err => {
+        done(err)
+      })
+  }))
 
   passport.use(new strategies.google({
     clientID: '796666915392-7fnpjjcjs9v6ek83343gs84gg80m9hdi.apps.googleusercontent.com',
